9eb9d8f1b48eeed94822cc70394a9deba4b720c8
Alexander Bowers <7319abowers@gmail.com>
2020-10-21 00:56:16 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -173,7 +173,7 @@ def team_names
 
   game_hash.each do |teams, value1|
     #binding.pry
-    game_hash[teams].each do |name, value2|
+    game_hash[teams].once do |name, value2|
       #binding.pry
       teams_array << value2
     end
